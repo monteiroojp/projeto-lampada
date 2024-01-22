3f34d719f50530604dbd741ff0c18ddbc66085a6
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-01-22 11:49:55 -0300

    lamp pt.01

---
+++ script.js
@@ -0,0 +1,32 @@
+var on = document.getElementById('on')
+on.addEventListener('click' , ligar)
+
+var of = document.getElementById('off')
+of.addEventListener('click', desligar)
+
+var img = document.getElementById('lamp')
+img.addEventListener('mouseenter' , ligar)
+img.addEventListener('mouseout' , desligar)
+img.addEventListener('dblclick' , quebrar)
+
+function lampestragada(){
+    return img.src.indexOf('quebrada') > -1
+}
+
+function ligar(){
+    if(!lampestragada() ){
+        img.src = 'imagens/ligada.jpg'
+    }
+}
+
+function desligar(){
+    if(!lampestragada()){
+        img.src = 'imagens/desligada.jpg'
+    }
+}
+
+function quebrar(){
+    if(!lampestragada()){
+        img.src = 'imagens/quebrada.jpg'
+    }
+}
